1bfdc644e7c6d45b635c60c32cdfc1aaa3b96e92
Murtazin <renatmurtazin@yahoo.com>
2022-04-02 12:47:53 +0300

    Initial Commit

---
+++ AnimationsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsApp
+//
+//  Created by Renat Murtazin on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
